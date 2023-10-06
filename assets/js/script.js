ae594b7cb835606209e7d75ab55de74b0db1b46b
Gar-16 <>
2023-10-06 02:45:49 +0000

    Add basic html and some js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let quizContainer = document.getElementById('quiz');
+let resultsContainer = document.getElementById('results');
+let submitButton = document.getElementById('submit');
+
+function buildQuiz() {}
+
+function showResults() {}
+
